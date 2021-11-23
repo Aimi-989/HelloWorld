3f5fb6feb9ff65a5c97bedd083b79002053b4172
Aimi <aimi.valerija.anderton@gmail.com>
2021-11-23 11:44:39 +0200

    HelloWorld

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+
+        int age;
+
+        byte numberByte = 10;
+        short numberShort = 11;
+        int numberInt = 12;
+        long numberLong = 13;
+        String name = "John";
+        float numberFloat = 11.2f;
+        double numberDouble = 10.3D;
+        boolean isVIP;
+
+        //isVIP = true;
+
+        //byte number1, number2;
+        //number1 = 12;
+        //int sum = 50+25;
+
+        System.out.println("Byte is" + numberByte);
+        System.out.println("Short is" + numberShort);
+        System.out.println("Integer is" + numberInt);
+        System.out.println("String is" + name);
+        System.out.println("Long is" + numberLong);
+        System.out.println("Float" + numberFloat);
+        System.out.println("Double " + numberDouble);
+
+    }
+}
